c37051fa84e5fb723b9ab48b86200f2a645810e9
Melen <spirit.of.melen@gmail.com>
2021-09-17 14:11:59 +0200

    initial-commit

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ from discord_webhook import DiscordWebhook, DiscordEmbed
 
 
 
-load_dotenv()
+load_dotenv() #Check Environment Variables in .env file
 project_id = os.getenv('PROJECT_ID')
 webhook_url= os.getenv('WEBHOOK')
 
 webhook = DiscordWebhook(url=webhook_url) #Discord WebHook URL
-w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/%s' %project_id)) #Infura Authentication
+w3 = Web3(Web3.HTTPProvider('https://arb1.arbitrum.io/rpc')) #Network RPC
 
-bb_address = w3.toChecksumAddress("0xf5bce5077908a1b7370b9ae04adc565ebd643966") #BentoBox Contract Address
-MIM_contract_address=w3.toChecksumAddress("0x99d8a9c45b2eca8864373a26d1459e3dff1e17f3") #MIM Contract Address
+bb_address = w3.toChecksumAddress("0x74c764D41B77DBbb4fe771daB1939B00b146894A") #BentoBox Contract Address
+MIM_contract_address=w3.toChecksumAddress("0xfea7a6a0b346362bf88a9e4a88416b77a57d6c2a") #MIM Contract Address
 bb_ABI = json.load(open('BentoBoxV1.json', 'r')) #BentoBox ABI load, from BentoBoxV1.json
 bentobox = w3.eth.contract(address=bb_address, abi=bb_ABI) 
 
@@ -33,10 +33,10 @@ def  getMIMAmount(mim_address, cauldron):
 def sendMessage(tokens, amount):
     amount=int(amount)
     amount=(f"{amount:,}")
-    embed=DiscordEmbed(title=cauldrons[tokens]['title'], url=cauldrons[tokens]['website'], color=0x37367b)
+    embed=DiscordEmbed(title=cauldrons[tokens]['title'], url=cauldrons[tokens]['website'], color=0xffffff)
     embed.set_thumbnail(url=cauldrons[tokens]['logo'])
-    embed.add_embed_field(name="%s Market Replenish" %tokens, value="%s MIMs are available for minting on ETH Mainnet! <:MIM:888155951016857662>" %amount, inline=False)
-    embed.set_footer(text=cauldrons[tokens]['footer'])
+    embed.add_embed_field(name="%s Market Replenish" %tokens, value="%s MIMs are available for minting on Arbitrum Mainnet! <:MIM:888155951016857662>" %amount, inline=False)
+    embed.set_footer(text="Arbitrum L2")
     webhook.add_embed(embed)
     response = webhook.execute(remove_embeds=True)
     sleep(5)
@@ -49,7 +49,7 @@ for tokens in cauldrons.keys():
     print("%s :" %tokens)
     print("Old amount : ", cauldrons[tokens]['previous_amount'])
     
-    if amount - Decimal(cauldrons[tokens]['previous_amount']) > 150000 and cauldrons[tokens]['monitored']==True: #test Balance increase
+    if amount - Decimal(cauldrons[tokens]['previous_amount']) > 100000 and cauldrons[tokens]['monitored']==True: #test Balance increase
         sendMessage(tokens, amount) #Send discord msg
     
     cauldrons[tokens]['previous_amount']=str(amount)
